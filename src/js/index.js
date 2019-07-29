e68149a636be2b25632e5a06c33a291b5b1b561e
HarryShaun <harryshaunwang@outlook.com>
2019-07-29 15:33:29 +0800

    add asynchronous function; hold-click function;

--- src/js/index.js
+++ src/js/index.js
@@ -5,42 +5,53 @@ const origin_color = "rgb(42, 42, 42)";
 //default speed, excursion and force
 const defaults = [5, 0, 5];
 
+let is_loop = 1;
+let is_stop = 1;
+let is_play = 0;
+
+let mouse_state = -1;
+
 //Two-dimensional array, if this button is chosen
 var chosen = new Array(matrix_col);
-for(let i = 0; i < matrix_col; i++) {
+for (let i = 0; i < matrix_col; i++) {
     chosen[i] = new Array(matrix_row);
-    for(let j = 0; j < matrix_row; j++) {
+    for (let j = 0; j < matrix_row; j++) {
         chosen[i][j] = 0;
     }
 }
 
 matrix_property = {
-    "m_row":                matrix_row,
-    "m_col":                matrix_col,
-    "matrix":               chosen
+    "m_row": matrix_row,
+    "m_col": matrix_col,
+    "matrix": chosen
 }
 
 instrument_property = {
-    "i_name":               "piano",
-    "speed":                5,
-    "excursion":             0,
-    "force":                5
+    "i_name": "piano",
+    "speed": 5,
+    "excursion": 0,
+    "force": 5
 }
 
 //render buttons on this page
 renderMatrix = () => {
-    for(let i = matrix_row - 1; i >= 0; i--) {
+    for (let i = matrix_row - 1; i >= 0; i--) {
         //a newdiv is a row
         let newdiv = document.createElement("div");
         let class_value = i + "-row rows";
         newdiv.setAttribute("class", class_value);
-        for(let j = 0; j < matrix_col; j++) {
+        for (let j = 0; j < matrix_col; j++) {
             let newbutton = document.createElement("button");
-            //add click event value
-            let click_value = "chooseButton(this)";
+            //add events value
+            //let click_value = "chooseButton(this)";
+            let mousedown_value = "mousedownButton(this)";
+            let mouseup_value = "mouseupButton(this)";
+            let mouseover_value = "mouseoverButton(this)";
             //add row and colum to class
             let class_value = i + "-" + j + " buttons";
-            newbutton.setAttribute("onclick", click_value);
+            newbutton.setAttribute("onmousedown", mousedown_value);
+            newbutton.setAttribute("onmouseup", mouseup_value);
+            newbutton.setAttribute("onmouseover", mouseover_value);
             newbutton.setAttribute("class", class_value);
             newdiv.appendChild(newbutton);
             document.getElementsByClassName("launchpad")[0].appendChild(newdiv);
@@ -52,24 +63,42 @@ renderMatrix();
 //get row number by classname
 getRow = (str) => {
     let t = 0;
-    while(str[t] != "-") {
+    while (str[t] != "-") {
         t++;
     }
     return str.slice(0, t);
 }
+
 //get colum number by classname
 getCol = (str) => {
     let t = str.length;
-    while(str[t] != "-") {
+    while (str[t] != "-") {
         t--;
     }
     return str.slice(t + 1, str.length - 8);
 }
+
 //change chosen array when you click a button
-chooseButton = (e) => {
+mousedownButton = (e) => {
     let row = getRow(e.className);
     let col = getCol(e.className);
-    if(chosen[col][row] === 0) {
+    if (chosen[col][row] === 0) {
+        chosen[col][row] = 1;
+        e.style.backgroundColor = "white"
+        mouse_state = 1;
+    }
+    else {
+        chosen[col][row] = 0;
+        e.style.backgroundColor = origin_color;
+        mouse_state = 0;
+    }
+    //tryPlaySound();
+}
+
+mouseupButton = (e) => {
+    let row = getRow(e.className);
+    let col = getCol(e.className);
+    if (chosen[col][row] === 0) {
         chosen[col][row] = 1;
         e.style.backgroundColor = "white"
     }
@@ -77,32 +106,69 @@ chooseButton = (e) => {
         chosen[col][row] = 0;
         e.style.backgroundColor = origin_color;
     }
+    mouse_state = -1;
+    //tryPlaySound();
 }
+
+mouseoverButton = (e) => {
+    let row = getRow(e.className);
+    let col = getCol(e.className);
+    if(mouse_state !== -1 && mouse_state !== chosen[col][row]) {
+        if (chosen[col][row] === 0) {
+            chosen[col][row] = 1;
+            e.style.backgroundColor = "white"
+        }
+        else {
+            chosen[col][row] = 0;
+            e.style.backgroundColor = origin_color;
+        }
+    }
+}
+
 //change property number when you slide the rollbar
 changeNumber = (e) => {
     e.nextElementSibling.innerHTML = e.value;
     let property = e.previousElementSibling.innerHTML.toLowerCase();
-    if(property === "speed") {
+    if (property === "speed") {
         instrument_property.speed = parseInt(e.value);
     }
-    else if(property === "excursion") {
+    else if (property === "excursion") {
         instrument_property.excursion = parseInt(e.value);
     }
-    else if(property === "force") {
+    else if (property === "force") {
         instrument_property.force = parseInt(e.value);
     }
+    tryPlaySound();
 }
+
+function switchPlayState() {
+    tryPlaySound();
+}
+
+function switchLoopState() {
+    is_loop = is_loop ^ 1;
+    if (document.getElementById('loop_state').innerHTML === "loop") {
+        document.getElementById('loop_state').innerHTML = "no loop";
+    }
+    else {
+        document.getElementById('loop_state').innerHTML = "loop";
+    }
+}
+
 //click clear button and reset everything to default
-clearAll = () => {
+function switchClearState() {
+    document.getElementById('loop_state').innerHTML = "loop";
+    is_loop = 0;
+    is_stop = 1;
     //clear buttons
-    for(let i = 0; i < matrix_col; i++) {
-        for(let j = 0; j < matrix_row; j++) {
+    for (let i = 0; i < matrix_col; i++) {
+        for (let j = 0; j < matrix_row; j++) {
             chosen[i][j] = 0;
             document.getElementsByClassName("buttons")[i * matrix_col + j].style.backgroundColor = origin_color;
         }
     }
     //clear property rollbars
-    for(let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
         let inputbutton = document.getElementsByClassName("roll_bar")[i].childNodes[3];
         inputbutton.value = defaults[i];
         document.getElementsByClassName("roll_bar")[i].childNodes[5].innerHTML = defaults[i];
@@ -112,9 +178,10 @@ clearAll = () => {
     instrument_property.excursion = 0;
     instrument_property.force = 5;
 }
+
 //click reset button and set properties to default
 resetDefault = () => {
-    for(let i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
         let inputbutton = document.getElementsByClassName("roll_bar")[i].childNodes[3];
         inputbutton.value = defaults[i];
         document.getElementsByClassName("roll_bar")[i].childNodes[5].innerHTML = defaults[i];
@@ -123,17 +190,34 @@ resetDefault = () => {
     instrument_property.excursion = 0;
     instrument_property.force = 5;
 }
+
 //set instrument when you click the instruments buttons
 setInstrument = (e) => {
     instrument_property.i_name = e.childNodes[1].innerHTML.toLowerCase();
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         document.getElementsByClassName("instrument")[i].classList.remove("currentInstrument");
     }
     //change the current instrument's style
     e.className += " currentInstrument";
 }
+
+function play_stellatrix_sound() {
+    let sound = make_stellatrix_sound(matrix_property, instrument_property);
+    play(sound);
+    is_play = 0;
+    if(is_loop === 1) tryPlaySound();
+}
+
 //when you click, play the music
 test = () => {
     let music = make_stellatrix_sound(matrix_property, instrument_property);
     play(music);
-} 
+}
+
+
+function tryPlaySound() {
+    if(is_play === 0) {
+        is_play === 1;
+        setTimeout(play_stellatrix_sound, 0);
+    }
+}
